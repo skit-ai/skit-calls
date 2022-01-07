e79f5db6bd4953d1495238c9f36825e0db220e84
ltbringer <amresh.venugopal@gmail.com>
2022-01-08 04:46:22 +0530

    chore: default const values.

--- skit_calls/constants.py
+++ skit_calls/constants.py
@@ -2,16 +2,17 @@ AUTHORIZATION = "authorization"
 ROUTE__CALL = "/call_report/calls/"
 ROUTE__TURN = "/call_report/calls/{}/"
 LIVE = "live"
+SUBTESTING = "subtesting"
 
-# --- Tagged calls are one of the following ----
+# ----------- Tagged calls are one of the following -----------
 REPORTED = "reported"
 RESOLVED = "resolved"
 # but if there are no preferences, we can get
 # all calls.
 ALL = "all"
-# -----------------------------------------------
+# ------------------------------------------------------------
 
-# ------------- Get call params -----------------
+# --------------------- Get call params ----------------------
 START_DATE = "start"
 END_DATE = "end"
 LANG_CODE = "lang_code"
@@ -25,8 +26,9 @@ CUSTOM_SEARCH_VALUE = "custom_search_value"
 
 # These are system generated pings and are never required
 DEFAULT_IGNORE_CALLERS_LIST = ["ev-connect", "0000000000"]
-# ------------------------------------------------
+# ---------------------------------------------------------
 
+# ----------- columns in the calls+turns list -------------
 TOTAL_PAGES = "total_pages"
 TOTAL_ITEMS = "total_items"
 ITEMS = "items"
@@ -39,7 +41,22 @@ PREDICTION = "prediction"
 METADATA = "metadata"
 CREATED_AT = "created_at"
 UPDATED_AT = "updated_at"
+# ---------------------------------------------------------
+
+# ------------------------- cli -----------------------------------
+DESCRIPTION = """
+Skit.ai's calls library.
+
+We provide means to sample calls and conversations from a 
+specified environment.
+
+Learn about this library at: https://github.com/skit-ai/skit-calls
+""".strip()
+# ------------------------------------------------------------------
 
 FILES = "files"
 IN_MEMORY = "in-memory"
-MEMORY_LIMIT = 10000
+MEMORY_LIMIT = 10e7
+DEFAULT_API_GATEWAY_URL = "https://apigateway.vernacular.ai"
+DEFAULT_CALL_QUANTITY = 200
+DEFAULT_TIMEZONE = "Asia/Kolkata"
